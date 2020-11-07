37d53ecc851267d2134caa6b5b68c2d6210d0486
Cleonildo Junior <cleu.junior@gmail.com>
2020-11-06 22:12:17 -0300

    Primeiro Commit

---
+++ admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/scripts.js"></script>
+</head>
+<?php include("includes/header.php");?>
+<?php include("includes/nav.php");?>
+
+
+
+	<div class="jumbotron">
+		<h1 class="text-center">Admin</h1>
+	</div>
+
+
+
+  <?php include("includes/footer.php");?>
